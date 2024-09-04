6cdc4a2a81cc6e27d11d0f2bfd500be90243c2d1
Rodrigo <rorro19vas@gmail.com>
2024-09-04 19:58:56 -0400

    Continuacion ejercicio jugadores

--- python 1/jugadores.py
+++ python 1/jugadores.py
@@ -1,11 +1,11 @@
 # Con la siguiente lista de diccionarios de jugadores de un equipo de fútbol,
 # filtra y muestra únicamente los jugadores que han jugado más de
-# 20 partidos y cuyo promedio de goles sea superior a 10. Jugadores2010
+# 20 partidos y cuyo promedio de goles sea superior a 10. Jugadores Poderosos
 
 # Además:
 # Calcula el promedio de goles de los jugadores que tienen más de
 # 20 partidos jugados, cuya posición en el campo sea "Delantero"
-# y cuya edad sea superior a 25 años.
+# y cuya edad sea superior a 25 años. Delanteros Infalibles
 
 jugadores = [
     {'nombre': 'Lionel', 'edad': 33, 'posicion': 'Delantero', 'partidos_jugados': 30, 'goles': [15, 12, 18]},
@@ -21,13 +21,11 @@ jugadores = [
     {'nombre': 'Piqué', 'edad': 33, 'posicion': 'Defensa', 'partidos_jugados': 31, 'goles': [3, 4, 5]},
     {'nombre': 'Kroos', 'edad': 30, 'posicion': 'Mediocampista', 'partidos_jugados': 30, 'goles': [6, 5, 4]},
     {'nombre': 'Alba', 'edad': 32, 'posicion': 'Defensa', 'partidos_jugados': 27, 'goles': [1, 2, 3]},
-    {'nombre': 'Rodrigo', 'edad': 22, 'posicion': 'Defensa', 'partidos_jugados': 5, 'goles': [10, 200, 3]},
 ]
 
 # 1. Identifica al defensa con más goles.
-# 2. Filtra y muestra a los jugadores con más de 25 partidos jugados y menos de 10 goles.
-# 3. Encuentra al jugador con el mejor promedio de goles por partido jugado.
-# 4. Calcula la edad promedio de los mediocampistas.
+# 2. Encuentra al jugador con el mejor promedio de goles por partido jugado. @Jugador Estrella
+# 3. Calcula la edad promedio de los mediocampistas.
 
 def obtenerPromedioGoles(listaGoles):
     sumaGoles = 0
@@ -42,29 +40,37 @@ def obtenerTotalGoles(listaGoles):
         sumaGoles =  sumaGoles + goles
     return sumaGoles
 
-jugadores2010 = []
-jugadores20Delanteros25 = []
-golesTotalesRecord = 0
-for jugador in jugadores:
-    promedioGoles = obtenerPromedioGoles(jugador['goles'])
-    golesTotalesJugador = obtenerTotalGoles(jugador['goles'])
-    if jugador['partidos_jugados'] > 20 and promedioGoles > 10:
-        jugadores2010.append(jugador['nombre'])
 
-    if jugador['partidos_jugados'] > 20 and jugador['posicion'] == 'Delantero' and jugador['edad'] > 25:
-        jugadorCaracteristicas = {'nombre':jugador['nombre'],'goles':promedioGoles}
-        jugadores20Delanteros25.append(jugadorCaracteristicas)
-    
-    if jugador['posicion'] == 'Defensa' and golesTotalesJugador > golesTotalesRecord:
-        defensaConMasGoles = jugador
-        golesTotalesRecord = golesTotalesJugador
+jugadoresPoderosos = []
+def mostrarJugadoresPoderosos():
+    for jugador in jugadores:
+        promedioGoles = obtenerPromedioGoles(jugador['goles'])
+        if jugador['partidos_jugados'] > 20 and promedioGoles > 10:
+            jugadoresPoderosos.append(jugador['nombre'])
+    print('A continuacion los jugadores poderosos: ')
+    print(jugadoresPoderosos)
+
+delanterosInfalibles = []
+def mostrarDelanterosInfalibles():
+    for jugador in jugadores:
+        promedioGoles = obtenerPromedioGoles(jugador['goles'])
+        if jugador['partidos_jugados'] > 20 and jugador['posicion'] == 'Delantero' and jugador['edad'] > 25:
+            nuevoDelanteroInfalible = {'nombre':jugador['nombre'],'goles':promedioGoles}
+            delanterosInfalibles.append(nuevoDelanteroInfalible)
+    print('A continuacion los delanteros infalibles: ')
+    print(delanterosInfalibles)
 
 
+def mostrarDefensaGoleador():
+    maximoGoles = 0
+    for jugador in jugadores:
+        golesJugador = obtenerTotalGoles(jugador['goles'])
+        if jugador['posicion'] == 'Defensa' and golesJugador > maximoGoles:
+            defensaGoleador = jugador
+            maximoGoles = golesJugador
+    print('Defensa con mas goles: ',defensaGoleador,'Goles: ',maximoGoles)
 
 
-print('Jugadores con más de 20 partidos y cuyo promedio de goles sea superior a 10')
-print(jugadores2010)
-print('Jugadores con más de 20 partidos que sean delanteros y que tengan +25')
-print(jugadores20Delanteros25)
-print('Defensa con mas goles: ',defensaConMasGoles,'Goles: ',golesTotalesRecord)
-
+mostrarJugadoresPoderosos()
+mostrarDelanterosInfalibles()
+mostrarDefensaGoleador()
