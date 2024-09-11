33873a43bd1fb253b2339d508de179050ff9ea76
Rodrigo <rorro19vas@gmail.com>
2024-09-11 19:19:21 -0300

    Ejercicio nombres y personas

---
+++ python 1/nombres.py
@@ -0,0 +1,14 @@
+# Con la siguiente lista de nombres
+# Debes añadir un "Hola" antes de cada nombre
+# Luego imprimir la lista por consola
+nombres = ["Carlos", "Luis", "Ana", "Marta", "Sofía"]
+
+largoLista = len(nombres)
+for indice in range(largoLista):
+    nombres[indice] = 'perrito' 
+print(nombres)
+
+"""
+Dada una lista de nombres tienes que pedirle al usuario que ingrese un apellido para cada nombre mientras se ejecuta el programa
+Luego imprimir esa lista de nombres actualizada con los apellidos
+"""
---
+++ python 1/personas.py
@@ -0,0 +1,15 @@
+personas = [
+    {"nombre": "Carlos", "edad": 17},
+    {"nombre": "Luis", "edad": 22},
+    {"nombre": "Ana", "edad": 15},
+    {"nombre": "Marta", "edad": 18},
+    {"nombre": "Sofía", "edad": 19}
+]
+
+"""
+Crea una función llamada clasificar_personas() que reciba la lista de personas y genere una nueva lista en la que,
+para cada persona, se incluya su nombre junto con el mensaje "es mayor de edad"
+o "es menor de edad", dependiendo de su edad.
+
+Finalmente imprime esta nueva lista por consola.
+"""
