28d5fefeaf0ff11c70b8c93a964c1c0de8f84ae0
Rodrigo <rorro19vas@gmail.com>
2024-09-27 21:58:38 -0300

    Repaso

--- python 2 (poo)/repaso s1-4/calculadora.py
+++ python 2 (poo)/repaso s1-4/calculadora.py
@@ -7,10 +7,53 @@ Recuerda validar la division. No se puede dividir por 0
 """
 
 class Calculadora:
+    #Metodo constructor que es HEREDADO a las demas clases hijas.
     def __init__(self,a,b):
         self.a = a
         self.b = b
 
     def calcular(self):
         pass
+
+class Suma(Calculadora):
+
+    def calcular(self):
+        suma = self.a + self.b
+        print(f'La suma entre {self.a} y {self.b} es: {suma}')
+
+class Resta(Calculadora):
+
+    def calcular(self):
+        resta = self.a - self.b
+        print(f'La resta entre {self.a} y {self.b} es: {resta}')
+
+class Multiplicacion(Calculadora):
+
+    def calcular(self):
+        producto = self.a * self.b
+        print(f'El producto entre {self.a} y {self.b} es: {producto}')
+
+class Division(Calculadora):
+
+    def calcular(self):  
+        if self.b == 0:
+            print ('Fatal error: No se puede dividir por 0 !!')
+        else:
+            cuociente = self.a / self.b
+            resto = self.a % self.b
+            print(f'El cuociente entre {self.a} y {self.b} es: {cuociente} y su resto es {resto}')
+
+sumita = Suma(10,2)
+sumita.calcular()
+
+restita = Resta(9,8)
+restita.calcular()
+
+multiplicacioncita = Multiplicacion(9,0)
+multiplicacioncita.calcular()
+
+divisioncita = Division(81,3)
+divisioncita.calcular()
+
+    
         
--- python 2 (poo)/repaso s1-4/empleado.py
+++ python 2 (poo)/repaso s1-4/empleado.py
@@ -7,3 +7,58 @@ estas clases. Teniendo en cuenta que, El desarrollador tiene un salario base, El
 
 Programa las clases y sus metodos correspondientes, empleando polimorfismo y herencia.
 """
+
+class Empleado():
+    #Metodo Constructor
+    def __init__(self,nombre,apellido,id) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+        self.id = id
+    
+    #Declarar metodo polimorfico
+    def calcular_pago(self):
+        pass
+
+class Desarrollador(Empleado):
+    #Metodo Constructor
+    def __init__(self, nombre, apellido, id, salarioBase) -> None:
+        super().__init__(nombre, apellido, id)
+        self.salario_base = salarioBase
+
+    def calcular_pago(self):
+        print(f'El salario del desarrollador es: {self.salario_base}')
+
+class Freelance(Empleado):
+    #Metodo Constructor
+    def __init__(self, nombre, apellido, id,horas_trabajadas, tarifa_por_hora ) -> None:
+        super().__init__(nombre, apellido, id)
+        self.horas_trabajadas = horas_trabajadas
+        self.tarifa_por_hora = tarifa_por_hora
+
+    def calcular_pago(self):
+        salario = self.horas_trabajadas * self.tarifa_por_hora
+        print(f'El salario del freelance es: {salario}')
+
+class Gerente(Empleado):
+    #Metodo Constructor
+    def __init__(self, nombre, apellido, id, salarioBase, bono) -> None:
+        super().__init__(nombre, apellido, id)
+        self.salario_base = salarioBase
+        self.bono = bono
+    
+    def calcular_pago(self):
+        salario = self.salario_base + self.bono
+        print(f'El salario del gerente es: {salario}')
+
+
+#Instancia de clases
+desarrolladorcito = Desarrollador('Rodrigo','Vasquez',1,12000)
+freenlancito = Freelance('Rodrigo','Morales',2,500,7000)
+gerentito = Gerente('Rodrigo','Perez',3,30000, 10)
+
+desarrolladorcito.calcular_pago()
+freenlancito.calcular_pago()
+gerentito.calcular_pago()
+
+
+        
