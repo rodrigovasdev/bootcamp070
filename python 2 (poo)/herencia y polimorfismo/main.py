ad43cb5242877ccf7b58e00e7a78c1bc099fded7
Rodrigo <rorro19vas@gmail.com>
2024-09-30 20:19:03 -0300

    Ejemplos Sesion 5

--- python 2 (poo)/herencia y polimorfismo/main.py
+++ python 2 (poo)/herencia y polimorfismo/main.py
@@ -1,4 +1,4 @@
-from persona import Persona, Supervisor, Cliente
+from persona import Persona, Supervisor, Cliente, SupervisorZona
 
 persona1 = Persona('Rodrigo', 'Vasquez','123456')
 persona2 = Persona('José', 'Sanchez','456789')
@@ -6,5 +6,11 @@ persona2 = Persona('José', 'Sanchez','456789')
 supervisor1 = Supervisor('Juan', 'Perez','123456','Sur')
 cliente1 = Cliente('José', 'Sanchez','456789',20)
 
-print(persona1.__dict__)
+superSupervisor = SupervisorZona('Rodrigo','Sanchez','109392820','Oriente',7,109,123)
+superSupervisor.imprimir_supervisor_zona()
+
+print(SupervisorZona.__mro__)
+print(Supervisor.__mro__)
+print(Persona.__mro__)
+
 
--- python 2 (poo)/herencia y polimorfismo/persona.py
+++ python 2 (poo)/herencia y polimorfismo/persona.py
@@ -10,7 +10,7 @@ class Persona:
         print('Soy de la clase Persona!', a)
 
     #APLICANDO POLIMORFISMO
-    def imprimir_datos(self):
+    def imprimir_persona(self):
         print(f'Nombre: {self.nombre} \nApellidos: {self.apellidos}\nCédula: {self.cedula} Datos mostrados desde Imprimir_Datos')
 
     def __str__(self):
@@ -27,8 +27,8 @@ class Supervisor(Persona):
         print('Soy de la clase Supervisor!', a)
 
     #APLICANDO POLIMORFISMO
-    def imprimir_datos(self):
-        super().imprimir_datos()
+    def imprimir_supervisor(self):
+        super().imprimir_persona()
         print(f'Zona: {self.zona}')
     
     def __str__(self):
@@ -47,10 +47,31 @@ class Cliente(Persona):
     
     #APLICANDO POLIMORFISMO
     def imprimir_datos(self):
-        super().imprimir_datos()
+        super().imprimir_persona()
         print(f'Descuento: {self.descuento}')
 
     def __str__(self):
         datos = super().__str__ + (f'\nDescuento: {self.descuento}')
         return datos
-    
+
+class Capacidades:
+    def __init__(self,ncertificados, raiting):
+        self.ncertificados = ncertificados
+        self.raiting = raiting
+
+    def imprimir_capacidades(self):
+        print( f'Nro Certificados: {self.ncertificados} \nRaiting:{self.raiting}')
+
+    def __str__(self):
+        return f'Numero de Certificados: {self.ncertificados}\nRaiting: {self.raiting}'
+
+class SupervisorZona(Supervisor, Capacidades):
+    def __init__(self, nombre, apellidos, cedula, zona,ncertificados, raiting, promedio):
+        Supervisor.__init__(self, nombre, apellidos, cedula, zona)
+        Capacidades.__init__(self, ncertificados, raiting)
+        self.promedio = promedio
+
+    def imprimir_supervisor_zona(self):
+        Supervisor.imprimir_supervisor(self)
+        Capacidades.imprimir_capacidades(self)
+        print('Promedio:',self.promedio)
