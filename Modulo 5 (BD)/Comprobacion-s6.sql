a106adb0d20d2d982a63e8b633693e442b0ccf09
ablancomsa <ablanco@msageoconsultores.com>
2024-11-08 21:55:04 -0300

    sql

---
+++ Modulo 5 (BD)/Comprobacion-s6.sql
@@ -0,0 +1,27 @@
+-- Req 1
+SELECT * FROM Vehiculo
+LEFT JOIN Venta ON vehiculo.idvehiculo = Venta.Vehiculo_IDVehiculo
+WHERE Venta.Vehiculo_IDVehiculo IS NULL
+
+-- Req 2
+SELECT  venta.folio AS folio,
+		venta.fecha AS FechaVenta, 
+		venta.monto AS MontoVenta, 
+		cliente.nombre AS NombreCliente, 
+		cliente.rut AS RutCliente, 
+		vehiculo.patente AS Patente, 
+		vehiculo.marca AS NombreMarca,
+		vehiculo.modelo AS Modelo
+FROM Venta
+JOIN Cliente
+ON Venta.cliente_RUT = cliente.RUT
+JOIN Vehiculo
+ON Venta.vehiculo_IDVehiculo = Vehiculo.IDVehiculo
+WHERE to_char("fecha",'YYYY-MM') = '2020-01'
+
+-- Req 3
+SELECT venta.fecha, vehiculo.marca, SUM(venta.monto) AS suma_ventas
+FROM venta
+JOIN vehiculo ON venta.Vehiculo_IDVehiculo = vehiculo.IDVehiculo
+WHERE to_char("fecha",'YYYY-MM') = '2020-01'
+GROUP BY venta.fecha, vehiculo.marca
--- Modulo 5 (BD)/ev-s5.sql
+++ Modulo 5 (BD)/ev-s5.sql
@@ -48,5 +48,51 @@ JOIN reparto ON pelicula.id=reparto.id_pelicula
 ORDER BY pelicula.estreno DESC
 LIMIT 4;
 
+-- Punto 12
+BEGIN;
+	INSERT INTO pelicula (id, titulo, estreno, director)
+	VALUES (108, 'Pelicula que no se vera', 2019, 'Anthony Russo');
+ROLLBACK;
+
+BEGIN;
+	INSERT INTO pelicula (id, titulo, estreno, director)
+	VALUES (109, 'Pelicula que SI se vera', 2024, 'Anthony Russo');
+COMMIT;
+
+-- Punto 13
+BEGIN;
+UPDATE public.pelicula
+	SET director= CASE director
+	WHEN 'Steven Spielberg' THEN 'H Simpson'
+	WHEN 'Ridley Scott' THEN 'Spiderman'
+	WHEN 'William Wyler' THEN 'Superman'
+	WHEN 'Hayao Miyazaki' THEN 'Batman'
+	WHEN 'Brad Bird' THEN 'Linterna Verde'
+	ELSE director
+	END;
+ROLLBACK;
+
+-- Punto 14
+BEGIN;
+SAVEPOINT sp;
+INSERT INTO public.reparto(
+	id_pelicula, actor)
+	VALUES (72, 'Rodrigo V'),(72, 'Don Quijote'),(72, 'Sancho Panza');
+COMMIT;
 
 
+-- 16.	Inserte 2 actores para cada película estrenada el 2001.
+
+-- Ids de peliculas estrenadas el 2001:
+-- 55
+-- 78
+-- 94
+-- 99
+-- 13
+-- 16
+INSERT INTO reparto VALUES('55','actor 1'),('55','actor 2'),
+   						  ('78','actor 3'),('78','actor 4'),
+   						  ('94','actor 5'),('94','actor 6'),
+   						  ('99','actor 7'),('99','actor 8'),
+   						  ('13','actor 9'),('13','actor 10'),
+   						  ('16','actor 11'),('16','actor 12');
