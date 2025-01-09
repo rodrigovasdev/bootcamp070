f07a06b9944f717987b18d7f42a8b194383e33b0
rodrigovasdev <rodrigo.vasqueze@gmail.com>
2025-01-09 20:32:02 -0300

    parte 4 Guia practica

--- python 4 (django avanzado)/gestion_biblioteca/biblioteca/models.py
+++ python 4 (django avanzado)/gestion_biblioteca/biblioteca/models.py
@@ -3,9 +3,16 @@ from django.db import models
 # Create your models here.
 class Biblioteca(models.Model):
     nombre = models.CharField(max_length=50)
+    ubicacion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
 
 class Autor(models.Model):
     nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=50)
@@ -13,3 +20,7 @@ class Libro(models.Model):
     biblioteca = models.ForeignKey(Biblioteca, on_delete=models.CASCADE)
     fecha_publicacion = models.DateField()
     estado = models.CharField(choices=(('DISPONIBLE', 'Disponible'), ('PRESTADO', 'Prestado')))
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.titulo
